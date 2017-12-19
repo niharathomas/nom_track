3a048d541675148b456f5332fe0db8523166fc98
Nihara Thomas <nthomas1@mediatemple.net>
2017-12-18 21:04:22 -0800

    Working default homepage after rebase

--- nom_track_app/app/food_truck_api.py
+++ nom_track_app/app/food_truck_api.py
@@ -2,11 +2,14 @@ import json
 
 from datetime import datetime
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_cache import Cache
 
 cache = Cache(config={'CACHE_TYPE': 'simple'})
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Flask(__name__,
+            static_folder="./dist/static",
+            template_folder="./dist")
 cache.init_app(app)
 
 
@@ -30,11 +33,6 @@ def get_food_trucks_for_day(dt):
     return example
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
 @app.route('/api/today', methods=['GET'])
 def list_today_options():
     """
@@ -44,6 +42,11 @@ def list_today_options():
     today = datetime.now().date()
     data = get_food_trucks_for_day(today)
     return json.dumps(data)
+
+# Homepage
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
